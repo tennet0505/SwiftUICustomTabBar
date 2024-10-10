6d707605d98f8edbb1980b6c28475e2b4ec45459
Oleg Ten <tennet0505@gmail.com>
2024-10-10 14:52:10 +0200

    Initial Commit

---
+++ JustApp/JustAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustAppApp.swift
+//  JustApp
+//
+//  Created by Oleg Ten on 10/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JustAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
